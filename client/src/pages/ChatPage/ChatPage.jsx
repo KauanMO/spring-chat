19f3115c4c7bcf8a0d0095130b40a555ee74f879
KauanMO <kauaanmatheus@gmail.com>
2024-01-24 12:51:41 -0300

    Sentinela de login em chatpage

--- client/src/pages/ChatPage/ChatPage.jsx
+++ client/src/pages/ChatPage/ChatPage.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom'
+import styles from './ChatPage.module.css'
 
 export const ChatPage = () => {
+    const navigate = useNavigate();
+
+    function verifyLogin() {
+        if (!sessionStorage.getItem('id')) {
+            navigate('/');
+        }
+    }
+
     return (
-        <div>
-            chatpage
+        <div className={styles.container} onMouseEnter={verifyLogin}>
+            
         </div>
     );
 }
