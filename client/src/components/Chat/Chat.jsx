6f7b4acd1c3aec56fe4a75288d3c2fbca50112cc
KauanMO <kauaanmatheus@gmail.com>
2024-01-24 15:25:18 -0300

    Usestate mensagem

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,19 +1,33 @@
 import { AiFillCaretRight } from "react-icons/ai";
-import React from "react";
+import React, { useState } from "react";
 import styles from './Chat.module.css'
 import { Input } from "../Input/Input";
 
 export const Chat = () => {
+    const [message, setMessage] = useState('');
+
     const SubmitButton = () => {
         return <div className={styles.submit_button}>
             <AiFillCaretRight />
         </div>
     }
 
+    const typingMessage = e => {
+        if (e.key === 'Enter') {
+            sendMessage();
+        }
+
+        setMessage(e.target.value);
+    }
+
+    const sendMessage = () => {
+        
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.chat_message}>
-                <Input type={'text'} label={'Digite uma mensagem'} styleClass={'chat_message'} submitButton={true}>
+                <Input type={'text'} handleOnKeyDown={typingMessage} label={'Digite uma mensagem'} styleClass={'chat_message'} submitButton={true}>
                     <SubmitButton />
                 </Input>
             </div>
--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from './Input.module.css'
 
-export const Input = ({ type, label, styleClass, handleOnChange, children }) => {
+export const Input = ({ type, label, styleClass, handleOnChange, children, handleOnKeyDown }) => {
     return (
         <div className={styles.container}>
-            <input onChange={handleOnChange} placeholder={label} className={`${styles[styleClass]} ${styles.input}`} type={type} />
+            <input onChange={handleOnChange} onKeyDown={handleOnKeyDown} placeholder={label} className={`${styles[styleClass]} ${styles.input}`} type={type} />
             {children}
         </div>
     );
