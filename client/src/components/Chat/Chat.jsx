24f969e809ba404ab3e3590bf2bb456466386826
KauanMO <kauaanmatheus@gmail.com>
2024-01-30 10:09:02 -0300

    Componente envio mensagem

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -7,7 +7,7 @@ export const Chat = () => {
     const [message, setMessage] = useState('');
 
     const SubmitButton = () => {
-        return <div className={styles.submit_button}>
+        return <div onClick={sendMessage} className={styles.submit_button}>
             <AiFillCaretRight />
         </div>
     }
@@ -20,14 +20,32 @@ export const Chat = () => {
         setMessage(e.target.value);
     }
 
-    const sendMessage = () => {
-        
+    const sendMessage = e => {
+        try {
+            fetch('http://localhost:8080/message', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    message,
+                    user: {
+                        userid: sessionStorage.getItem('id')
+                    }
+                })
+            });
+
+            setMessage('');
+            document.querySelector('#chat_message').value = '';
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     return (
         <div className={styles.container}>
             <div className={styles.chat_message}>
-                <Input type={'text'} handleOnKeyDown={typingMessage} label={'Digite uma mensagem'} styleClass={'chat_message'} submitButton={true}>
+                <Input name={'chat_message'} type={'text'} handleOnKeyDown={typingMessage} label={'Digite uma mensagem'} styleClass={'chat_message'} submitButton={true}>
                     <SubmitButton />
                 </Input>
             </div>
--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from './Input.module.css'
 
-export const Input = ({ type, label, styleClass, handleOnChange, children, handleOnKeyDown }) => {
+export const Input = ({ type, label, styleClass, handleOnChange, children, handleOnKeyDown, name }) => {
     return (
         <div className={styles.container}>
-            <input onChange={handleOnChange} onKeyDown={handleOnKeyDown} placeholder={label} className={`${styles[styleClass]} ${styles.input}`} type={type} />
+            <input name={name} id={name} onChange={handleOnChange} onKeyDown={handleOnKeyDown} placeholder={label} className={`${styles[styleClass]} ${styles.input}`} type={type} />
             {children}
         </div>
     );
