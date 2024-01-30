d139efac4b0ba9d5522876391b4814112414fa6b
KauanMO <kauaanmatheus@gmail.com>
2024-01-30 15:33:53 -0300

    Configuração de websocket spring

--- springchat/src/main/java/com/springchat/springchat/controllers/MessageController.java
+++ springchat/src/main/java/com/springchat/springchat/controllers/MessageController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
