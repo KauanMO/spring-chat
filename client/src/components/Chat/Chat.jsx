225f820aae12287849cc83b54b1931b4c824a766
KauanMO <kauaanmatheus@gmail.com>
2024-01-31 13:52:56 -0300

    Avanço apresentação de mensagem no chat

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import styles from './Chat.module.css'
 import { Input } from "../Input/Input";
 
-
 export const Chat = ({ stompClient, messages }) => {
     const [message, setMessage] = useState('');
     console.log(messages);
@@ -34,7 +33,11 @@ export const Chat = ({ stompClient, messages }) => {
     const Message = ({ m }) => {
         return (
             <div>
-                <span>{m.message}</span>
+                <span className={m.user.userid == sessionStorage.getItem('id')
+                    ? styles.my_message
+                    : styles.other_message}>
+                    {m.message}
+                </span>
             </div>
         )
     }
